8b866ce0f297236bfefb05a97c7e17d9022f491b
jonathanko500 <jkko9010@gmail.com>
2021-02-09 19:33:37 -0800

    lab 2.34

---
+++ Lab/2.34/Source.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{//start main
+	string name, place, noun;
+	int num;
+
+	cin >> name;
+	cin >> place;
+	cin >> num;
+	cin >> noun;
+
+	cout << name << " went to " << place << " to buy " << num;
+	cout << " different types of " << noun;
+	cout << "." << endl;
+}//end main
