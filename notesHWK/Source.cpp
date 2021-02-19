5167a92ecc16cd9b75e9c117ad8fdb3c13e6ddb6
jonathanko500 <jkko9010@gmail.com>
2021-02-18 21:56:05 -0800

    hwk notes
    6.1 - 6.4

--- notesHWK/Source.cpp
+++ notesHWK/Source.cpp
@@ -8,37 +8,25 @@ using namespace std;
 int main()
 {
     const int NUM_VALS = 4;
-    vector<int> courseGrades(NUM_VALS);
-    int i;
+    unsigned int i;
+    vector<int> hourlyTemp(NUM_VALS);
 
-    for (i = 0; i < courseGrades.size(); ++i)
-    {
-        cin >> courseGrades.at(i);
+    for (i = 0; i < hourlyTemp.size(); ++i) {
+        cin >> hourlyTemp.at(i);
     }
 
-    for (i = 0; i < courseGrades.size(); ++i)
+    for (i = 0; i < hourlyTemp.size(); ++i)
     {
-        if (i < courseGrades.size() - 1)
+        if (i == hourlyTemp.size() - 1)
         {
-            cout << courseGrades.at(i) << " ";
+            cout << hourlyTemp.at(i);
         }
         else
         {
-            cout << courseGrades.at(i) << " ";
+            cout << hourlyTemp.at(i) << ", ";
         }
     }
-    cout << endl;
-    for (i = courseGrades.size() - 1; i > -1; --i)
-    {
-        if (i == 0)
-        {
-            cout << courseGrades.at(i) << " ";
-        }
-        else
-        {
-            cout << courseGrades.at(i) << " ";
-        }
-    }
+
     cout << endl;
 
     return 0;
