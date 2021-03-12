fae6399eeacf9f00d363185df58aedd97da521e7
jonathanko500 <jkko9010@gmail.com>
2021-03-11 20:32:59 -0800

    lab 7.6
    progress

--- Lab/7.6/Source.cpp
+++ Lab/7.6/Source.cpp
@@ -18,13 +18,14 @@ bool operator<=(const TokenFreq& o1, const TokenFreq& o2);
 const TokenFreq operator+ (const TokenFreq& o1, const TokenFreq o2);
 ostream& operator<<(ostream& os, const TokenFreq& o1);
 void getTokenFreqVec(string& istr, vector<TokenFreq>& tfVec);
-
+void selectionSort(vector<TokenFreq>& tokFreqVector);
 
 
 
 int main()
 {//start main
 
+	
 	//start test matrixInit
 	int row, col;
 	vector <vector<int> > list;
@@ -89,14 +90,40 @@ int main()
 	string text;
 	cin >> text;
 	getTokenFreqVec(text, key);
+	cout << key.size() << endl;
 	for (int i = 0; i < key.size(); i++)
 	{
-		cout << i << " = (token = \"" << key[i].token << "\", freq = " << key[i].freq << ")" << endl;
+		cout << i << " = (token = \"" << key.at(i).token << "\", freq = " << key.at(i).freq << ")" << endl;
 	}
 	//end getTokenFreqVec test
+	
 
+	//start selection sort test
 
+	//declare vectors + struct
+	vector<TokenFreq> sortS;
 
+	
+	TokenFreq one;
+	one.token = "one";
+	one.freq = 1;
+	sortS.push_back(one);
+
+	TokenFreq thirty;
+	one.token = "thirty";
+	one.freq = 30;
+	sortS.push_back(thirty);
+
+	TokenFreq k;
+	one.token = "1k";
+	one.freq = 1000;
+	sortS.push_back(k);
+
+	selectionSort(sortS);
+	for (int i = 0; i < sortS.size(); i++)
+	{
+		cout << sortS.at(i).token << " and " << sortS.at(i).freq << endl;
+	}
 
 }//end main
 
@@ -146,7 +173,6 @@ void getTokenFreqVec(string& istr, vector<TokenFreq>& tfVec)
 	TokenFreq tf;
 	while (stream >> temp)
 	{//start look through stream
-		
 		for (auto& key : temp)
 		{//makes temp lower case
 			key = tolower(key);
@@ -154,21 +180,42 @@ void getTokenFreqVec(string& istr, vector<TokenFreq>& tfVec)
 		
 		bool loop = false;//prevents repeat temp
 		for (auto& lock : tfVec)
-		{
+		{//start compares + count to check # of freq of token
 			if (lock.token == temp)
 			{
 				lock.freq++;
 				loop = true;
 			}
-		}
+		}//end compare + count
 		if (!loop)
-		{
+		{//puts token and freq in vector
 			tf.token = temp;
 			tf.freq = 1;
 			tfVec.push_back(tf);
 		}
 	}///end look through stream
 }//end func
+
+void selectionSort(vector<TokenFreq>& tokFreqVector)
+{//start selection sort
+	int min;
+	TokenFreq temp;
+	for (int i = 0; i < tokFreqVector.size(); i++)
+	{//start look through vector
+		min = i;
+		for (int j = i + 1; j < tokFreqVector.size(); j++)
+		{//start compare
+			if (tokFreqVector[j].freq < tokFreqVector[min].freq)
+			{//compare to see if the i+1 < i
+				min = j;
+			}
+		}//end compare
+		if (min > i)
+		{
+			swap(tokFreqVector[i], tokFreqVector[min]);
+		}
+	}//end look through vector
+}//end selection sort
 
 
 
