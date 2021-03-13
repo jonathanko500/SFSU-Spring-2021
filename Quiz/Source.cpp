4af57ad5bfe41b31561b44d95393a33de4d312e0
jonathanko500 <jkko9010@gmail.com>
2021-03-12 20:08:39 -0800

    quiz 2

--- Quiz/Source.cpp
+++ Quiz/Source.cpp
@@ -6,24 +6,13 @@
 using namespace std;
 
 
+
+
+
+
 int main()
 {
-    int sum = 0, row = 0, col = 0;
-    int count = 0;
-    for (row = 0; row < 6; row = row + 2)
-    {
-        for (col = row; col < 5; col = col + 1)
-        {
-            sum += row * col;
-            count++;
-        }
-    }
-
-    cout << count << endl;
-    cout << sum;
-
-            
-
-
     return 0;
 }
+
+
