99949a684b3aa9286e845bd78333f179d2a6aa27
jonathanko500 <jkko9010@gmail.com>
2021-04-11 20:11:39 -0700

    testFiles`
    parse specific data into vectors

--- testFiles/Main.cpp
+++ testFiles/Main.cpp
@@ -1,15 +1,13 @@
 #include <iostream>
+#include "cab.h"
 #include <fstream>
-#include "Rides.h"
+
 using namespace std;
 
 int main()
 {//start main
-	ifstream ride;
-	ride.open("cab_rides.csv",ios::in);
-	if (!ride.is_open())
-	{
-		cout << "file !open";
-	}
+	CabRides car;
 
+	car.vectorDataInsert();
+	
 }//end main
---
+++ testFiles/cab.h
@@ -0,0 +1,38 @@
+#ifndef CAB_H
+#define CAB_H
+
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class CabRides
+{
+public:
+    //travel distances ride
+    //float
+    vector <string> distance;
+
+    //prices of service
+    //float
+    vector <string> price;
+
+    //time stamp of each ride 
+    //int
+    vector <string> time_stamp;
+
+    //specific product purchased
+    vector <string> product_name;
+
+    //uber vs lyft
+    vector <string> cab_type;
+
+    //every other uneeded data pt
+    vector <string> misc;
+    //function to insert data from csv file 
+
+    void vectorDataInsert();
+
+};
+
+#endif
