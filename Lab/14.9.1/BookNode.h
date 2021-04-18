003776a5d4b58dc1bbddec0d30e56fdcea9f3352
jonathanko500 <jkko9010@gmail.com>
2021-04-17 22:04:44 -0700

    lab 14.9
    start with making header files

---
+++ Lab/14.9.1/BookNode.h
@@ -0,0 +1,35 @@
+#pragma once
+#include <string>
+using namespace std;
+
+class BookNode {
+public:
+    BookNode();
+
+    // Constructor
+    BookNode(string bookTitleInit, string bookAuthorInit, long bookISBNInit);
+
+    // Constructor
+    BookNode(string bookTitleInit, string bookAuthorInit, long bookISBNInit, BookNode* nextLoc);
+
+    // inserAfter
+    void insertAfter(BookNode* nodeLoc);
+
+    //setNext
+    void SetNext(BookNode* nodeLoc);
+
+    // Get location pointed by nextNodePtr
+    BookNode* GetNext() const;
+
+    long GetBookISBN() const;
+
+    // Print book information
+    void PrintBookInfo() const;
+
+private:
+    string bookTitle;
+    string bookAuthor;
+    long bookISBN;
+    BookNode* nextNodePtr; // Reference to the next node
+};
+#endif
