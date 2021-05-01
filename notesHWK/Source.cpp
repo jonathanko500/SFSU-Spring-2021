76088487bf40b895e838c0caf791ce1422361744
jonathanko500 <jkko9010@gmail.com>
2021-04-30 20:22:57 -0700

    inheritance

--- notesHWK/Source.cpp
+++ notesHWK/Source.cpp
@@ -10,3 +10,35 @@
 
 using namespace std;
 
+class FoodOrder {
+public:
+    FoodOrder(string foodType);
+    static int GetNextId();
+private:
+    string type = "None";
+    int id = 0;
+    static int nextId;
+};
+
+FoodOrder::FoodOrder(string foodType) {
+    type = foodType;
+    id = nextId;
+
+    nextId += 1;
+}
+
+int FoodOrder::GetNextId() {
+    return nextId;
+}
+
+int FoodOrder::nextId = 50;
+
+int main() {
+    FoodOrder order1("Sushi");
+    FoodOrder order2("Salad");
+    FoodOrder order3("Muffins");
+
+    cout << "Next ID: " << FoodOrder::GetNextId() << endl;
+
+    return 0;
+}
