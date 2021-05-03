686d0d092cf32b7ad0868beaa69c260b48d18772
jonathanko500 <jkko9010@gmail.com>
2021-05-02 19:18:16 -0700

    14.16
    nothing

--- Lab/14.16/VectorADT.cpp
+++ Lab/14.16/VectorADT.cpp
@@ -13,6 +13,7 @@ VectorADT::VectorADT() :size(0), capacity(10)
 	}
 }//end
 
+/*
 //big 3
 
 //destructor
@@ -51,9 +52,12 @@ void VectorADT::operator= (const VectorADT& copy)
 		array[i] = copy.array[i];
 	}
 }//end
+*/
+
+
 
 //other functions
-
+/*
 //push_back
 void VectorADT::push_back(double val)
 {//start
@@ -169,6 +173,8 @@ VectorADT VectorADT:: operator+ (VectorADT add)
 	return plus;
 }//end
 
+*/
+
 //operator <<
 ostream& operator<<(ostream& out, const VectorADT& pt)
 {//start
@@ -179,8 +185,10 @@ ostream& operator<<(ostream& out, const VectorADT& pt)
 	return out;
 }//end
 
+/*
 //curr capacity
 int VectorADT::curr_capacity() const
 {//start
 	return capacity;
 }//end
+*/
