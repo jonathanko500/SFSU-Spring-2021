1137d7b7b9afa43d489b0052e310679e49b877ad
jonathanko500 <jkko9010@gmail.com>
2021-05-18 16:50:50 -0700

    final
    implementation of a hypothetical class

---
+++ Final/Final/Customer.h
@@ -0,0 +1,25 @@
+#ifndef Customer_h
+#define Customer_h
+
+#include <iostream>
+#include "Item.h"
+#include <vector>
+
+using namespace std;
+
+class Customer
+{//start class
+private:
+    vector<Item> receipt;
+    int numItems;
+
+public:
+    /*
+    functions to be implemented if class properly
+    made
+    */
+};//end class
+
+#endif
+
+
---
+++ Final/Final/Item.h
@@ -0,0 +1,16 @@
+#ifndef Item_h
+#define Item_h
+
+#include <iostream>
+
+using namespace std;
+
+class Item
+{//start class
+private:
+
+public:
+
+};//end class
+
+#endif
